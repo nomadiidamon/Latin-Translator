1dfcbc0a390f85c4c6a4794262e2c2d5f209b8f9
Damon Green II <dman1175@hotmail.com>
2024-04-25 01:49:52 -0400

    extra translations added

--- Latin Translator/Noun.h
+++ Latin Translator/Noun.h
@@ -7,6 +7,7 @@
 
 #define FIR_ENDS
 
+
 #define SEC_ENDS
 
 #define THI_ENDS
@@ -33,13 +34,46 @@ public:
 	std::string PluralEnding_w_Macron() const {
 
 	}
-	std::string Translation1() const {
+	std::string SingularTranslation1() const {
 
 	}
-	std::string Translation2() const {
+	std::string SingularTranslation2() const {
 
 	}
-	std::string Translation3() const {
+	std::string SingularTranslation3() const {
+
+	}
+	std::string SingularTranslation4() const {
+
+	}
+	std::string SingularTranslation5() const {
+
+	}
+	std::string SingularTranslation6() const {
+
+	}
+	std::string SingularTranslation7() const {
+
+	}
+	std::string PluralTranslation1() const {
+
+	}
+	std::string PluralTranslation2() const {
+
+	}
+	std::string PluralTranslation3() const {
+
+	}
+	std::string PluralTranslation4() const {
+
+	}
+	std::string PluralTranslation5() const {
+
+	}
+	std::string PluralTranslation6() const {
+
+	}
+	std::string PluralTranslation7() const {
 
 	}
 	std::string Explanation() const {
@@ -60,28 +94,80 @@ public:
 	void SetPluralEnding_w_Macron() {
 
 	}
-	void SetTranslation1(std::string addition) {
+	void SetSingularTranslation1(std::string addition) {
 
 	}
-	void SetTranslation2(std::string addition) {
+	void SetSingularTranslation2(std::string addition) {
 
 	}
-	void SetTranslation3(std::string addition) {
+	void SetSingularTranslation3(std::string addition) {
 
 	}
-	void SetExplanation()  {
+	void SetSingularTranslation4(std::string addition) {
+
+	}	
+	void SetSingularTranslation5(std::string addition) {
+
+	}	
+	void SetSingularTranslation6(std::string addition) {
+
+	}	
+	void SetSingularTranslation7(std::string addition) {
+
+	}
+	void SetPluralTranslation1(std::string addition) {
+
+	}
+	void SetPluralTranslation2(std::string addition) {
+
+	}
+	void SetPluralTranslation3(std::string addition) {
+
+	}
+	void SetPluralTranslation4(std::string addition) {
+
+	}	
+	void SetPluralTranslation5(std::string addition) {
+
+	}
+	void SetPluralTranslation6(std::string addition) {
+
+	}
+	void SetPluralTranslation7(std::string addition) {
+
+	}
+	void SetExplanation() {
 
 	}
 
 private:
+
+	// Add four more translations for sing and plural
+
 	std::string singularEnding = "";
 	std::string singularEnding_w_macron = "";
 	std::string pluralEnding = "";
 	std::string pluralEnding_w_macron = "";
-	std::string translation1 = "";
-	std::string translation2 = "";
-	std::string translation3 = "";
+	std::string singularTranslation1 = "";
+	std::string singularTranslation2 = "";
+	std::string singularTranslation3 = "";
+	std::string singularTranslation4 = "";
+	std::string singularTranslation5 = "";
+	std::string singularTranslation6 = "";
+	std::string singularTranslation7 = "";
+
+	std::string pluralTranslation1 = "";
+	std::string pluralTranslation2 = "";
+	std::string pluralTranslation3 = "";
+	std::string pluralTranslation4 = "";
+	std::string pluralTranslation5 = "";
+	std::string pluralTranslation6 = "";
+	std::string pluralTranslation7 = "";
+
 	std::string explanation = "";
+
+
+
 };
 
 struct Declension {
@@ -175,56 +261,74 @@ struct Declension {
 		if (declension == "First") {
 			Nom.SetSingularEnding('a');
 			Nom.SetSingularEnding_w_Macron();
-			Nom.SetPluralEnding();
+			Nom.SetPluralEnding('ae');
 			Nom.SetPluralEnding_w_Macron();
-			Nom.SetTranslation1();
-			Nom.SetTranslation2();
-			Nom.SetTranslation3();
+			Nom.SetSingularTranslation1("a");
+			Nom.SetSingularTranslation2("an");
+			Nom.SetSingularTranslation3("the");
+			Nom.SetPluralTranslation1("the");
+			Nom.SetPluralTranslation2("");
+			Nom.SetPluralTranslation3("");
 			Nom.SetExplanation();
 
 			Gen.SetSingularEnding('ae');
 			Gen.SetSingularEnding_w_Macron();
-			Gen.SetPluralEnding();
+			Gen.SetPluralEnding('arum');
 			Gen.SetPluralEnding_w_Macron();
-			Gen.SetTranslation1();
-			Gen.SetTranslation2();
-			Gen.SetTranslation3();
+			Nom.SetSingularTranslation1("of the");
+			Nom.SetSingularTranslation2("of a");
+			Nom.SetSingularTranslation3("'s");
+			Nom.SetPluralTranslation1("of the");
+			Nom.SetPluralTranslation2("'s");
+			Nom.SetPluralTranslation3("");
 			Gen.SetExplanation();
 
 			Dat.SetSingularEnding('ae');
 			Dat.SetSingularEnding_w_Macron();
-			Dat.SetPluralEnding();
+			Dat.SetPluralEnding('is');
 			Dat.SetPluralEnding_w_Macron();
-			Dat.SetTranslation1();
-			Dat.SetTranslation2();
-			Dat.SetTranslation3();
+			Nom.SetSingularTranslation1("to the");
+			Nom.SetSingularTranslation2("for the");
+			Nom.SetSingularTranslation3("to a");
+			Nom.SetPluralTranslation1("for the");
+			Nom.SetPluralTranslation2("to the");
+			Nom.SetPluralTranslation3("");
 			Dat.SetExplanation();
 
 			Acc.SetSingularEnding('am');
 			Acc.SetSingularEnding_w_Macron();
 			Acc.SetPluralEnding();
 			Acc.SetPluralEnding_w_Macron();
-			Acc.SetTranslation1();
-			Acc.SetTranslation2();
-			Acc.SetTranslation3();
+			Nom.SetSingularTranslation1("A");
+			Nom.SetSingularTranslation2("An");
+			Nom.SetSingularTranslation3("");
+			Nom.SetPluralTranslation1("The");
+			Nom.SetPluralTranslation2("");
+			Nom.SetPluralTranslation3("");
 			Acc.SetExplanation();
 
 			Abl.SetSingularEnding('a');
 			Abl.SetSingularEnding_w_Macron();
 			Abl.SetPluralEnding();
 			Abl.SetPluralEnding_w_Macron();
-			Abl.SetTranslation1();
-			Abl.SetTranslation2();
-			Abl.SetTranslation3();
+			Nom.SetSingularTranslation1("A");
+			Nom.SetSingularTranslation2("An");
+			Nom.SetSingularTranslation3("");
+			Nom.SetPluralTranslation1("The");
+			Nom.SetPluralTranslation2("");
+			Nom.SetPluralTranslation3("");
 			Abl.SetExplanation();
 
 			Voc.SetSingularEnding('a');
 			Voc.SetSingularEnding_w_Macron();
 			Voc.SetPluralEnding();
 			Voc.SetPluralEnding_w_Macron();
-			Voc.SetTranslation1();
-			Voc.SetTranslation2();
-			Voc.SetTranslation3();
+			Nom.SetSingularTranslation1("A");
+			Nom.SetSingularTranslation2("An");
+			Nom.SetSingularTranslation3("");
+			Nom.SetPluralTranslation1("The");
+			Nom.SetPluralTranslation2("");
+			Nom.SetPluralTranslation3("");
 			Voc.SetExplanation();
 		}
 		else if (declension == "Second") {
